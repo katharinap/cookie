dee7f931ad93b3f7a4a8ac7432c95d77993d467e
Katharina Peter <gaz@gmx.net>
2014-10-01 22:14:52 -0700

    added picture to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,10 +6,12 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  picture    :string
 #
 
 class Recipe < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
+  mount_uploader :picture, PictureUploader
 
   has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: proc { |attributes| attributes['value'].blank? }
@@ -20,6 +22,11 @@ class Recipe < ActiveRecord::Base
   has_many :steps, dependent: :destroy
   accepts_nested_attributes_for :steps, allow_destroy: true, reject_if: proc { |attributes| attributes['description'].blank? }
   
+  # returns the basename of the picture file if present or nil
+  def picture_file_name
+    picture? ? File.basename(picture.to_s) : nil
+  end
+
   def prepare_recipe(params)
     %i(name directions).each do |attr|
       self.send("#{attr}=", params[attr].strip) unless params[attr].blank?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915042918) do
+ActiveRecord::Schema.define(version: 20141002050243) do
 
   create_table "ingredients", force: true do |t|
     t.integer  "recipe_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140915042918) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   create_table "references", force: true do |t|
