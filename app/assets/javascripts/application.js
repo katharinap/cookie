be20d8e8c14f640aace620888a4590a17c27e47e
Katharina Peter <gaz@gmx.net>
2014-10-18 16:50:56 -0700

    added jquery-turbolinks to fix issues with bootstrap.file-input.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require jquery_nested_form
 //= require local_time
 //= require_tree .
+//= require turbolinks
